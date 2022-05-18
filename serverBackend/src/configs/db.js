af9d0cbe883b70314341b6ebfcc5fd03425a80e4
PranavDalvi9 <96105012+PranavDalvi9@users.noreply.github.com>
2022-05-19 00:39:39 +0530

    Update backend

--- serverBackend/src/configs/db.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = () => {
-  return mongoose.connect("mongodb+srv://pranav:pranav123@cluster0.gvulk.mongodb.net/E-CommerceWebsite?retryWrites=true&w=majority");
-};
