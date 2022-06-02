34b0bbdb8959d3acbe0f2ff6a7628c1916b4448f
PranavDalvi9 <96105012+PranavDalvi9@users.noreply.github.com>
2022-06-02 19:56:56 +0530

    Media Query

--- client/src/Components/DeliveryOptions/DeliveryOptions.jsx
+++ client/src/Components/DeliveryOptions/DeliveryOptions.jsx
@@ -4,7 +4,7 @@ import "./DeliveryOptions.css"
 export default function DeliveryOptions() {
     return (
         <div className='DeliveryOptions'>
-
+ 
             <div>
                 <div><img src="https://preview.colorlib.com/theme/capitalshop/assets/img/icon/services1.svg" alt="" /></div>
                 <p className='DeliveryP1'>Fast & Free Delivery</p>
